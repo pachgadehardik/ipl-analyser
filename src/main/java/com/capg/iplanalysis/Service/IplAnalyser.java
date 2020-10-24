b3175b4fcc04760d6163d8de8d48b96102aac1fc
pachgadehardik <pachgadehardik@gmail.com>
2020-10-24 07:11:38 +0530

    Refactored the analyser class with generic loading and sorting

--- src/main/java/com/capg/iplanalysis/Service/IplAnalyser.java
+++ src/main/java/com/capg/iplanalysis/Service/IplAnalyser.java
@@ -16,6 +16,7 @@ import com.capg.csvbuilder.ICSVBuilder;
 import com.capg.iplanalysis.Exceptions.IplAnalysisException;
 import com.capg.iplanalysis.Exceptions.IplAnalysisException.IplExceptionType;
 import com.capg.iplanalysis.Pojos.MostRuns;
+import com.capg.iplanalysis.Pojos.MostWickets;
 import com.capg.iplanalysis.enums.PlayerType;
 import com.capg.iplanalysis.enums.SortingFieldType;
 
@@ -23,23 +24,25 @@ public class IplAnalyser<T> {
 
 	private PlayerType playerType;
 	private static final Logger logger = LogManager.getLogger(IplAnalyser.class);
-	List<MostRuns> playerStatsList = null;
+	List<T> playerStatsList;
 
 	public IplAnalyser(PlayerType playerType) {
 		this.playerType = playerType;
 	}
 
-	public <T> List<MostRuns> loadCSVFile(String iplMostRunsCsvFilePath) throws IplAnalysisException {
+	public <T> List<T> loadCSVFile(String iplMostRunsCsvFilePath) throws IplAnalysisException {
 		DOMConfigurator.configure("log4j.xml");
 
 		try (Reader reader = Files.newBufferedReader(Paths.get(iplMostRunsCsvFilePath));) {
 			ICSVBuilder csvBuilder = null;
 
 			csvBuilder = CSVBuilderFactory.createCSVBuilder();
-			if (playerType == PlayerType.BATSMAN) {
+			if (this.playerType == PlayerType.BATSMAN) {
 				playerStatsList = csvBuilder.getCSVFileList(reader, MostRuns.class);
 			}
-			return playerStatsList;
+			else if(this.playerType == PlayerType.BOWLER)
+				playerStatsList = csvBuilder.getCSVFileList(reader, MostWickets.class);
+			return (List<T>) playerStatsList;
 		} catch (IOException e) {
 			throw new IplAnalysisException(IplExceptionType.FILE_NOT_FOUND_TYPE, e.getMessage());
 		} catch (RuntimeException r) {
@@ -53,13 +56,13 @@ public class IplAnalyser<T> {
 
 	}
 
-	public List<MostRuns> getSortedDataByField(List<MostRuns> censusCSVList, SortingFieldType fieldType)
+	public List<T> getSortedDataByField(List<T> censusCSVList, SortingFieldType fieldType)
 			throws IplAnalysisException {
 
 		if (playerStatsList == null || playerStatsList.size() == 0)
 			throw new IplAnalysisException(IplExceptionType.OTHER_TYPE, "DATA is Empty");
 		Collections.sort(playerStatsList, fieldType.getComparator());
-		return playerStatsList;
+		return (List<T>) playerStatsList;
 	}
 
 }
