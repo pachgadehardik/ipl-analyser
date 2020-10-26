93aadd596ca7e867f4573c8e899e7239dad1dcd1
pachgadehardik <pachgadehardik@gmail.com>
2020-10-26 21:11:10 +0530

    Added Factory Builder class

--- src/main/java/com/capg/iplanalysis/Service/IplAnalyseBuilder.java
+++ src/main/java/com/capg/iplanalysis/Service/IplAnalyseBuilder.java
@@ -5,19 +5,15 @@ import com.capg.iplanalysis.Pojos.MostWickets;
 import com.capg.iplanalysis.enums.PlayerType;
 
 public class IplAnalyseBuilder {
-	
-	
-	
 
 	public static <T> Class<T> getClass(PlayerType playerType) {
-		
-		if(playerType == PlayerType.BATSMAN)
+
+		if (playerType == PlayerType.BATSMAN)
 			return (Class<T>) MostRuns.class;
-		else if(playerType == PlayerType.BOWLER)
+		else if (playerType == PlayerType.BOWLER)
 			return (Class<T>) MostWickets.class;
 		return null;
-		
-	}
-		
+
 	}
 
+}
